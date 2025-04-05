96ea2aac393923ba628b3e62c5253bb853a1b425
Paweł Skierś <pawelsk269@gmail.com>
2025-04-05 19:33:09 +0200

    Add evaluation

--- model/BiLSTM-dynamic-att-LSTM/script/BiLSTM-dynamic-att-LSTM-Torch.py
+++ model/BiLSTM-dynamic-att-LSTM/script/BiLSTM-dynamic-att-LSTM-Torch.py
@@ -4,6 +4,7 @@
 import torch
 import time
 import os
+from datetime import datetime
 from torch.cuda import is_available
 import torch.nn as nn
 import torch.optim as optim
@@ -91,12 +92,13 @@ class NERDataset(Dataset):
 # Collate Function
 def collate_fn(batch):
     token_ids, char_ids, labels = zip(*batch)
-    
+    lengths = torch.tensor([len(tokens) for tokens in token_ids])
+
     token_ids = nn.utils.rnn.pad_sequence(token_ids, batch_first=True, padding_value=word_vocab['PAD'])
     char_ids = nn.utils.rnn.pad_sequence([torch.tensor(c) for c in char_ids], batch_first=True, padding_value=char_vocab['PAD'])
     labels = nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=label_vocab['O'])
     
-    return token_ids, char_ids, labels
+    return token_ids, char_ids, labels, lengths
 
 # Dataloaders
 train_loader = DataLoader(NERDataset(train_data), batch_size=16, shuffle=True, collate_fn=collate_fn)
@@ -173,11 +175,177 @@ if torch.cuda.is_available():
     model = model.cuda()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Training Loop
-for epoch in range(150):
-    for step, (input_ids, char_ids, labels) in enumerate(train_loader):
+metric = ChunkEvaluator(label_list=label_vocab.keys(), suffix=True)
+
+@torch.no_grad()
+def predict(text):
+    tokens = text.split()
+    lens = len(tokens)
+    token_ids = convert_tokens_to_ids(tokens, word_vocab)
+    char_ids = [convert_word_to_char_ids(word, char_vocab) for word in tokens]
+    token_ids, char_ids = torch.tensor(token_ids).unsqueeze(0), torch.tensor(char_ids).unsqueeze(0)
+    if torch.cuda.is_available():
+        token_ids, char_ids = token_ids.cuda(), char_ids.cuda()
+
+    model.eval()
+    output = model(token_ids, char_ids)
+
+    preds = output.cpu().numpy().tolist()[0]
+
+    reverse_label_map = id2label
+    preds = [reverse_label_map[pred] for pred in preds]
+    return tokens, preds
+
+entity_counts = {}
+for entity_type in entity_types:
+    entity_counts[entity_type] = {"TP": 0, "FP": 0, "FN": 0}
+
+relation_counts = {}
+for relation_type in relation_types:
+    relation_counts[relation_type] = {"TP": 0, "FP": 0, "FN": 0}
+
+@torch.no_grad()
+def evaluate_classification(model, file_path):
+    model.eval()
+    lines = [i.strip('\n') for i in open(file_path, 'r', encoding='utf-8').readlines()]
+    for line in tqdm(lines):
+        tmp_list = line.split('\t')
+        sentence = tmp_list[0]
+        tag = tmp_list[1]
+        sentence_list = sentence.split()
+        tag_list = tag.split()
+
+        sent, tags = predict(' '.join(sentence_list))
+        tag_list = tag_list[0:len(tags)]
+        for index, tag in enumerate(tag_list):
+            if tag == 'O' and tags[index] == 'O':
+                continue
+            else:
+                if tag == 'O':
+                    entity_type1 = tags[index].split('_')[1]
+                    entity_counts[entity_type1]['FP'] += 1
+                elif tags[index] == 'O':
+                    entity_type2 = tag.split('_')[1]
+                    entity_counts[entity_type2]['FN'] += 1
+                elif tag.split('_')[1] == tags[index].split('_')[1]:
+                    entity_type = tag.split('_')[1]
+                    entity_counts[entity_type]['TP'] += 1
+                else:
+                    entity_type3 = tags[index].split('_')[1]
+                    entity_counts[entity_type3]['FN'] += 1
+
+                    entity_type4 = tag.split('_')[1]
+                    entity_counts[entity_type4]['FP'] += 1
+
+                if len(tag.split('_')) >= 3 and len(tags[index].split('_')) >= 3:
+                    if tag.split('_')[2] == tags[index].split('_')[2]:
+                        relation_type = tag.split('_')[2]
+                        relation_counts[relation_type]['TP'] += 1
+                    else:
+                        if tag == 'O':
+                            relation_type1 = tags[index].split('_')[2]
+                            relation_counts[relation_type1]['FP'] += 1
+                        elif tags[index] == 'O':
+                            relation_type2 = tag.split('_')[2]
+                            relation_counts[relation_type2]['FN'] += 1
+                        else:
+                            relation_type3 = tags[index].split('_')[2]
+                            relation_counts[relation_type3]['FN'] += 1
+
+                            relation_type4 = tag.split('_')[2]
+                            relation_counts[relation_type4]['FP'] += 1
+                elif len(tag.split('_')) == 2 and len(tags[index].split('_')) >= 3:
+                    relation_type1 = tags[index].split('_')[2]
+                    relation_counts[relation_type1]['FP'] += 1
+                elif len(tag.split('_')) >= 3 and len(tags[index].split('_')) == 2:
+                    relation_type2 = tag.split('_')[2]
+                    relation_counts[relation_type2]['FN'] += 1
+    evaluation_parameters = {}
+    print("-----------------Evaluate entities---------------------")
+    for entity_type in entity_counts:
+        TP = entity_counts[entity_type]["TP"]
+        FP = entity_counts[entity_type]["FP"]
+        FN = entity_counts[entity_type]["FN"]
+        if TP + FP == 0:
+            precision = 0.0
+        else:
+            precision = TP / (TP + FP)
+        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        if precision + recall == 0:
+            F1 = 0.0
+        else:
+            F1 = 2 * precision * recall / (precision + recall)
+        evaluation_parameters[entity_type] = []
+        evaluation_parameters[entity_type].extend([precision * 100, recall * 100, F1])
+        wandb.log(
+            {
+                f"entities/{entity_type}_precision": precision * 100,
+                f"entities/{entity_type}_recall": recall * 100,
+                f"entities/{entity_type}_F1": F1 * 100,
+            }
+        )
+        print("{}: precision={:.2f}%, recall={:.2f}%, F1={:.2f}%".format(entity_type, precision * 100, recall * 100,
+                                                                         F1 * 100))
+
+    print("-----------------Evaluate relation type---------------------")
+    for relation_type in relation_counts:
+        TP = relation_counts[relation_type]["TP"]
+        FP = relation_counts[relation_type]["FP"]
+        FN = relation_counts[relation_type]["FN"]
+        if TP + FP == 0:
+            precision = 0.0
+        else:
+            precision = TP / (TP + FP)
+        recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+        if precision + recall == 0:
+            F1 = 0.0
+        else:
+            F1 = 2 * precision * recall / (precision + recall)
+        evaluation_parameters[relation_type] = []
+        evaluation_parameters[relation_type].extend([precision * 100, recall * 100, F1])
+        wandb.log(
+            {
+                f"relation/{relation_type}_precision": precision * 100,
+                f"relation/{relation_type}_recall": recall * 100,
+                f"relation/{relation_type}_F1": F1 * 100,
+            }
+        )
+        print("{}: precision={:.2f}%, recall={:.2f}%, F1={:.2f}%".format(relation_type, precision * 100, recall * 100,
+                                                                         F1 * 100))
+    model.train()
+
+@torch.no_grad()
+def evaluate_v1(model, metric, data_loader):
+    model.eval()
+    metric.reset()
+    losses = []
+    for input_ids, char_ids, labels, lengths in data_loader:
         if torch.cuda.is_available():
-          input_ids, char_ids, labels = input_ids.cuda(), char_ids.cuda(), labels.cuda()
+            input_ids, char_ids, labels = input_ids.cuda(), char_ids.cuda(), labels.cuda()
+        loss = model(input_ids, char_ids, labels)
+        losses.append(loss.cpu().numpy())
+        preds = model(input_ids, char_ids, None)
+        n_infer, n_label, n_correct = metric.compute(lengths, preds, labels)
+        metric.update(n_infer.numpy(), n_label.numpy(), n_correct.numpy())
+    precision, recall, f1_score = metric.accumulate()
+    wandb.log({"val/precision": precision, "val/recall": recall, "val/F1": f1_score})
+    print(f'[EVAL] Precision: {precision:.6f} - Recall: {recall:.6f} - F1: {f1_score:.6f}')
+    avg_loss = np.mean(losses)
+    return avg_loss, precision, recall, f1_score
+
+date = datetime.today().strftime('%Y-%m-%d')
+nowname = f"BiLSTM-dynamic-att-LSTM_{date}"
+wandb.init(
+    name=nowname,
+    project="SPZC",
+)
+
+# Training Loop
+best_f1 = 0
+for epoch in range(150):
+    for step, (input_ids, char_ids, labels, lengths) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            input_ids, char_ids, labels = input_ids.cuda(), char_ids.cuda(), labels.cuda()
 
         model.train()
         optimizer.zero_grad()
@@ -189,5 +357,11 @@ for epoch in range(150):
             print(f"Epoch {epoch}, Step {step}, Loss: {loss.item():.4f}")
             wandb.log({"loss": loss.item()})
 
+    avg_loss, precision, recall, f1 = evaluate_v1(model, metric, dev_loader)
+    if f1 > best_f1 and f1 >= 0.4:
+        evaluate_classification(model, 'data1/dev.txt')
+        torch.save(model.state_dict(), f'checkpoints/{nowname}_best.pth')
+        best_f1 = f1
+
 # Save Model
-torch.save(model.state_dict(), 'model/bilstm_crf.pth')
+torch.save(model.state_dict(), f'checkpoints/{nowname}.pth')
